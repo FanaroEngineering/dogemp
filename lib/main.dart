ecef3cf2d360d6e020d3c944234b589a9aa887b9
Philippe Fanaro <philippefanaro@gmail.com>
2022-03-02 16:07:36 -0300

    better scrolling behavior with a mouse

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'widgets/custom_scroll_behavior.dart';
+
 import 'screens/home_screen.dart';
 
 void main() => runApp(const Dogemp());
@@ -13,6 +15,7 @@ class Dogemp extends StatelessWidget {
     return MaterialApp(
       title: 'DOGemP',
       theme: ThemeData.light(),
+      scrollBehavior: CustomScrollBehavior(),
       home: const HomeScreen(),
     );
   }
