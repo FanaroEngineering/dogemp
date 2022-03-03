6b145239116517547c3846f21ba60cc787ee0800
Philippe Fanaro <philippefanaro@gmail.com>
2022-03-03 15:09:04 -0300

    added a month and year title to the leagues

--- lib/screens/league_screen.dart
+++ lib/screens/league_screen.dart
@@ -45,126 +45,149 @@ class LeagueScreen extends StatelessWidget {
             scrollDirection: Axis.horizontal,
             child: Container(
               margin: const EdgeInsets.symmetric(horizontal: 20),
-              child: Row(
+              child: Column(
                 children: [
-                  DataTable(
-                    headingTextStyle: const TextStyle(
-                      fontSize: 15,
+                  const SizedBox(height: 30),
+                  const SelectableText(
+                    '2021',
+                    style: TextStyle(
+                      fontSize: 20,
                       fontWeight: FontWeight.bold,
+                      color: Colors.orange,
                     ),
-                    columnSpacing: 15,
-                    columns: _generateGroupHeader(
-                      level: 'A',
-                      players: [
-                        'ps',
-                        'Af',
-                        'Ph',
-                        'Pe',
-                        'Au',
-                      ],
-                    ),
-                    rows: [
-                      DataRow(
-                        color:
-                            MaterialStateProperty.resolveWith((_) => Colors.grey.withOpacity(0.1)),
-                        cells: [
-                          PlayerCell(playerName: 'psygo'),
-                          GameResultCell(text: 'X'),
-                          GameResultCell(text: 'V', gameRecordId: '38733948'),
-                          GameResultCell(text: 'V', gameRecordId: '38953919'),
-                          GameResultCell(text: 'V', gameRecordId: '38650411'),
-                          GameResultCell(text: 'V', gameRecordId: '38321258'),
-                        ],
-                      ),
-                      DataRow(
-                        cells: [
-                          PlayerCell(playerName: 'AfricanGrimReaper'),
-                          GameResultCell(text: 'D', gameRecordId: '38733948'),
-                          GameResultCell(text: 'X'),
-                          GameResultCell(text: '—'),
-                          GameResultCell(text: 'V', gameRecordId: '38650411'),
-                          GameResultCell(text: '—'),
-                        ],
-                      ),
-                      DataRow(
-                        color:
-                            MaterialStateProperty.resolveWith((_) => Colors.grey.withOpacity(0.1)),
-                        cells: [
-                          PlayerCell(playerName: 'Phelan'),
-                          GameResultCell(text: 'D', gameRecordId: '38953919'),
-                          GameResultCell(text: '—'),
-                          GameResultCell(text: 'X'),
-                          GameResultCell(text: '—'),
-                          GameResultCell(text: '—'),
-                        ],
-                      ),
-                      DataRow(
-                        cells: [
-                          PlayerCell(playerName: 'Pedepano'),
-                          GameResultCell(text: 'D', gameRecordId: '38482719'),
-                          GameResultCell(text: 'D', gameRecordId: '38650411'),
-                          GameResultCell(text: '—'),
-                          GameResultCell(text: 'X'),
-                          GameResultCell(text: '—'),
-                        ],
-                      ),
-                      DataRow(
-                        color:
-                            MaterialStateProperty.resolveWith((_) => Colors.grey.withOpacity(0.1)),
-                        cells: [
-                          PlayerCell(playerName: 'AudreyLucianoFilho'),
-                          GameResultCell(text: 'D', gameRecordId: '38321258'),
-                          GameResultCell(text: '—'),
-                          GameResultCell(text: '—'),
-                          GameResultCell(text: '—'),
-                          GameResultCell(text: 'X'),
-                        ],
-                      ),
-                    ],
                   ),
-                  const SizedBox(width: 50),
-                  DataTable(
-                    headingTextStyle: const TextStyle(fontWeight: FontWeight.bold),
-                    columnSpacing: 15,
-                    columns: const [
-                      DataColumn(label: SelectableText('Colocação')),
-                      DataColumn(label: SelectableText('Nome')),
-                    ],
-                    rows: [
-                      DataRow(
-                        color:
-                            MaterialStateProperty.resolveWith((_) => Colors.grey.withOpacity(0.1)),
-                        cells: [
-                          const DataCell(Center(child: SelectableText('1'))),
-                          PlayerCell(playerName: 'psygo'),
+                  const SizedBox(height: 30),
+                  const SelectableText(
+                    'Novembro',
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.deepOrange,
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  Row(
+                    children: [
+                      DataTable(
+                        headingTextStyle: const TextStyle(
+                          fontSize: 15,
+                          fontWeight: FontWeight.bold,
+                        ),
+                        columnSpacing: 15,
+                        columns: _generateGroupHeader(
+                          level: 'A',
+                          players: [
+                            'ps',
+                            'Af',
+                            'Ph',
+                            'Pe',
+                            'Au',
+                          ],
+                        ),
+                        rows: [
+                          DataRow(
+                            color: MaterialStateProperty.resolveWith(
+                                (_) => Colors.grey.withOpacity(0.1)),
+                            cells: [
+                              PlayerCell(playerName: 'psygo'),
+                              GameResultCell(text: 'X'),
+                              GameResultCell(text: 'V', gameRecordId: '38733948'),
+                              GameResultCell(text: 'V', gameRecordId: '38953919'),
+                              GameResultCell(text: 'V', gameRecordId: '38650411'),
+                              GameResultCell(text: 'V', gameRecordId: '38321258'),
+                            ],
+                          ),
+                          DataRow(
+                            cells: [
+                              PlayerCell(playerName: 'AfricanGrimReaper'),
+                              GameResultCell(text: 'D', gameRecordId: '38733948'),
+                              GameResultCell(text: 'X'),
+                              GameResultCell(text: '—'),
+                              GameResultCell(text: 'V', gameRecordId: '38650411'),
+                              GameResultCell(text: '—'),
+                            ],
+                          ),
+                          DataRow(
+                            color: MaterialStateProperty.resolveWith(
+                                (_) => Colors.grey.withOpacity(0.1)),
+                            cells: [
+                              PlayerCell(playerName: 'Phelan'),
+                              GameResultCell(text: 'D', gameRecordId: '38953919'),
+                              GameResultCell(text: '—'),
+                              GameResultCell(text: 'X'),
+                              GameResultCell(text: '—'),
+                              GameResultCell(text: '—'),
+                            ],
+                          ),
+                          DataRow(
+                            cells: [
+                              PlayerCell(playerName: 'Pedepano'),
+                              GameResultCell(text: 'D', gameRecordId: '38482719'),
+                              GameResultCell(text: 'D', gameRecordId: '38650411'),
+                              GameResultCell(text: '—'),
+                              GameResultCell(text: 'X'),
+                              GameResultCell(text: '—'),
+                            ],
+                          ),
+                          DataRow(
+                            color: MaterialStateProperty.resolveWith(
+                                (_) => Colors.grey.withOpacity(0.1)),
+                            cells: [
+                              PlayerCell(playerName: 'AudreyLucianoFilho'),
+                              GameResultCell(text: 'D', gameRecordId: '38321258'),
+                              GameResultCell(text: '—'),
+                              GameResultCell(text: '—'),
+                              GameResultCell(text: '—'),
+                              GameResultCell(text: 'X'),
+                            ],
+                          ),
                         ],
                       ),
-                      DataRow(
-                        cells: [
-                          const DataCell(Center(child: SelectableText('2'))),
-                          PlayerCell(playerName: 'AfricanGrimReaper'),
+                      const SizedBox(width: 50),
+                      DataTable(
+                        headingTextStyle: const TextStyle(fontWeight: FontWeight.bold),
+                        columnSpacing: 15,
+                        columns: const [
+                          DataColumn(label: SelectableText('Colocação')),
+                          DataColumn(label: SelectableText('Nome')),
                         ],
-                      ),
-                      DataRow(
-                        color:
-                            MaterialStateProperty.resolveWith((_) => Colors.grey.withOpacity(0.1)),
-                        cells: [
-                          const DataCell(Center(child: SelectableText('3'))),
-                          PlayerCell(playerName: 'Pedepano'),
-                        ],
-                      ),
-                      DataRow(
-                        cells: [
-                          const DataCell(Center(child: SelectableText('4'))),
-                          PlayerCell(playerName: 'Phelan'),
-                        ],
-                      ),
-                      DataRow(
-                        color:
-                            MaterialStateProperty.resolveWith((_) => Colors.grey.withOpacity(0.1)),
-                        cells: [
-                          const DataCell(Center(child: SelectableText('5'))),
-                          PlayerCell(playerName: 'AudreyLucianoFilho'),
+                        rows: [
+                          DataRow(
+                            color: MaterialStateProperty.resolveWith(
+                                (_) => Colors.grey.withOpacity(0.1)),
+                            cells: [
+                              const DataCell(Center(child: SelectableText('1'))),
+                              PlayerCell(playerName: 'psygo'),
+                            ],
+                          ),
+                          DataRow(
+                            cells: [
+                              const DataCell(Center(child: SelectableText('2'))),
+                              PlayerCell(playerName: 'AfricanGrimReaper'),
+                            ],
+                          ),
+                          DataRow(
+                            color: MaterialStateProperty.resolveWith(
+                                (_) => Colors.grey.withOpacity(0.1)),
+                            cells: [
+                              const DataCell(Center(child: SelectableText('3'))),
+                              PlayerCell(playerName: 'Pedepano'),
+                            ],
+                          ),
+                          DataRow(
+                            cells: [
+                              const DataCell(Center(child: SelectableText('4'))),
+                              PlayerCell(playerName: 'Phelan'),
+                            ],
+                          ),
+                          DataRow(
+                            color: MaterialStateProperty.resolveWith(
+                                (_) => Colors.grey.withOpacity(0.1)),
+                            cells: [
+                              const DataCell(Center(child: SelectableText('5'))),
+                              PlayerCell(playerName: 'AudreyLucianoFilho'),
+                            ],
+                          ),
                         ],
                       ),
                     ],
