03037321856f667d9c877ad611f98369b2f8cbf3
Philippe Fanaro <philippefanaro@gmail.com>
2022-03-16 20:38:31 -0300

    first steps towards link previews | ref #6

--- lib/widgets/clickable_link.dart
+++ lib/widgets/clickable_link.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/gestures.dart';
+import 'package:http/http.dart';
 
 import 'package:url_launcher/url_launcher.dart';
 
 import '../schema/links.dart';
 
 @immutable
-class ClickableLink extends StatelessWidget {
+class ClickableLink extends StatefulWidget {
   const ClickableLink({
     Key? key,
     required this.link,
@@ -19,12 +20,47 @@ class ClickableLink extends StatelessWidget {
   final Color color;
 
   @override
+  State<ClickableLink> createState() => _ClickableLinkState();
+}
+
+class _ClickableLinkState extends State<ClickableLink> {
+  Widget hoverWidget = const SizedBox.shrink();
+
+  void _fetchPreview(PointerEvent pointerEvent) {
+    setState(() {
+      if (widget.link.host == 'online-go.com' && widget.link.prePath == 'game') {
+        hoverWidget = Positioned(
+          top: 25,
+          child: Image.network('https://online-go.com/api/v1/games/${widget.link.id}/png'),
+        );
+      }
+    });
+  }
+
+  void _onExit(PointerEvent pointerEvent) {
+    setState(() {
+      hoverWidget = const SizedBox.shrink();
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return SelectableText.rich(
-      TextSpan(
-        text: linkText ?? link.id,
-        style: TextStyle(color: color),
-        recognizer: TapGestureRecognizer()..onTap = () async => launch(link.completeLink),
+    return MouseRegion(
+      onHover: _fetchPreview,
+      onExit: _onExit,
+      child: Stack(
+        clipBehavior: Clip.none,
+        children: [
+          SelectableText.rich(
+            TextSpan(
+              text: widget.linkText ?? widget.link.id,
+              style: TextStyle(color: widget.color),
+              recognizer: TapGestureRecognizer()
+                ..onTap = () async => launch(widget.link.completeLink),
+            ),
+          ),
+          hoverWidget,
+        ],
       ),
     );
   }
